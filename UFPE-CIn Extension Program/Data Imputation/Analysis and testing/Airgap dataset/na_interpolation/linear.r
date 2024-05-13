c023bb8bfb2df07d730e88dfa4f476a34023ccdf
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2024-05-13 14:40:29 -0300

    Update and rename UFPE-CIn Extension Program/Data Imputation/Analysis and testing/na_interpolation/linear.r to UFPE-CIn Extension Program/Data Imputation/Analysis and testing/Airgap dataset/na_interpolation/linear.r

--- UFPE-CIn Extension Program/Data Imputation/Analysis and testing/Airgap dataset/na_interpolation/linear.r
+++ UFPE-CIn Extension Program/Data Imputation/Analysis and testing/Airgap dataset/na_interpolation/linear.r
@@ -16,6 +16,6 @@ MSE <- mean((dataset_predicted - dataset_complete)^2) # Mean Squared Error
 
 RMSE <- sqrt(mean((dataset_predicted - dataset_complete)^2)) # Root Mean Squared Error
 
-print(paste("MAE:", MAE))
-print(paste("MSE:", MSE))
-print(paste("RMSE:", RMSE))
+print(paste("MAE:", MAE)) # 1.39707190361673
+print(paste("MSE:", MSE)) # 30.2940495582614
+print(paste("RMSE:", RMSE)) # 5.50400304853307
